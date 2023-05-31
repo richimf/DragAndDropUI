77f31bc53ad286456072632882745c77c1478ab2
Ricardo Montesinos <ricardo.montesinosfernandez@mx.bosch.com>
2023-05-31 08:27:07 -0600

    Main project

---
+++ DragAndDropper/DragAndDropper/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  DragAndDropper
+//
+//  Created by Ricardo on 31/05/23.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+}
+
